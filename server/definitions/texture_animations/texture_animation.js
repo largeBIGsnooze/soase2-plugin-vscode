715fb8b29ff3fce55a6ff904bf45ac9345a15a45
Tyloth <bruceunotbusiness59@gmail.com>
2024-04-12 20:32:06 +0000

    Removed unused inheritance

--- server/definitions/texture_animations/texture_animation.js
+++ server/definitions/texture_animations/texture_animation.js
@@ -1,11 +1,8 @@
 const { schema, integer, vecInt2 } = require('../data_types')
-const Definitions = require('../definitions')
 
-module.exports = class TextureAnimation extends Definitions {
+module.exports = class TextureAnimation {
     /* eslint-disable no-unused-vars */
     constructor({ fileText: fileText, fileExt: fileExt, fileName: fileName }, diagnostics, gameInstallationFolder, cache) {
-        super(gameInstallationFolder)
-
         this.cache = cache
     }
 
